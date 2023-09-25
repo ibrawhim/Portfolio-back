a6fb6aaaf66e4ac413d33fd9ae9c2ffe186cebfc
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2023-09-25 20:39:02 +0100

    portfolio

--- app.js
+++ app.js
@@ -13,33 +13,6 @@ app.use('/contact',contactRouter)
 
 
 
-// var transporter = nodemailer.createTransport({
-//     host: 'smtp.gmail.com',
-//     port: 465,
-//     secure: true,
-//     auth: {
-//       user: 'ibrahimabiodun069@gmail.com',
-//       pass: 'xiimfxbozayirwud'
-//     },
-//     tls: {
-//         rejectUnauthorized: false, // ignore SSL certificate errors
-//     }
-//   });
-  
-//   var mailOptions = {
-//     from: 'ibrahimabiodun069@gmail.com',
-//     to: 'adeniyi.ibrawhim@yahoo.com',
-//     subject: 'You Checked Ibrahim Portfolio',
-//     text: 'Your message was received and will get back to you as soon as possible!'
-//   };
-  
-//   transporter.sendMail(mailOptions, function(error, info){
-//     if (error) {
-//       console.log(error);
-//     } else {
-//       console.log('Email sent: ' + info.response);
-//     }
-//   });
 
 app.listen('4444',()=>{
     console.log('listening here');
